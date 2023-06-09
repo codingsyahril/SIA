a1eaedb43fa44eb53fe75dd4f0cd51626aa5ca0e
unknown <syahril.digital@gmail.com>
2023-06-09 11:14:59 +0800

    update content_header + KAS KELUAR

--- app/kas_keluar.php
+++ app/kas_keluar.php
@@ -88,7 +88,7 @@
                           <div class="modal-dialog modal-lg">
                             <div class="modal-content">
                               <div class="modal-header">
-                                <h4 class="modal-title">TAMBAH KAS MASUK</h4>
+                                <h4 class="modal-title">TAMBAH KAS KELUAR</h4>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                   <span aria-hidden="true">&times;</span>
                                 </button>
